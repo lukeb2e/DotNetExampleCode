aeb7a91f8fefa920139aa3684d276df5316d30c5
Christian Roschat <croscha@emea.corpdir.net>
2021-09-08 15:44:31 +0200

    added basic project

---
+++ DotNetDemo/Controllers/Habitants.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace DotNetDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HabitantsController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<Habitant> Get()
+        {
+            var habitants = getAllHabitantsasList();
+            throw new NotImplementedException();
+        }
+        
+        private List<Habitant> getAllHabitantsasList()
+        {
+            //var filePath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory), @"/DataBase/Habitants.json");
+            var filePath = Path.GetFullPath("DataBase/Habitants.json");
+           var habitants = System.IO.File.ReadAllText(filePath);
+
+           return JsonConvert.DeserializeObject<List<Habitant>>(habitants);
+
+        }
+    }
+
+
+}
